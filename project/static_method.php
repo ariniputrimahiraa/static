b527b20283b2ac39daf9bd438da53c9f77108442
raraharsa <aputrimahira@gmail.com>
2026-04-22 20:03:34 +0700

    upp3

--- project/static_method.php
+++ project/static_method.php
@@ -5,12 +5,14 @@ class Matematika {
     public static function kali($a, $b) {
         return $a * $b;
     }
-
     public static function bagi($a, $b) {
         return $a / $b;
     }
     public static function tambah($a, $b) {
         return $a + $b;
+    }
+    public static function kurang($a, $b) {
+        return $a - $b;
     }
 
 }
@@ -20,5 +22,7 @@ echo "<br>";
 echo Matematika::bagi(10, 2);
 echo "<br>";
 echo Matematika::tambah(3, 3);
+echo "<br>";
+echo Matematika::kurang(10, 2);
 
 ?>
